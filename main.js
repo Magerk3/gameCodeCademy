6d96be63928bd185e3e617af1c0b5a77185b08ce
Nikolai <magerk38@gmail.com>
2022-09-14 04:43:28 +0800

    sadf

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+
 
 class Field {
-    constructor(field) {
+    constructor(field, row, column) {
             this.field = field;
-            this.row = 0;
-            this.column = 0;
+            this.row = row;
+            this.column = column;
+            this.multiplier = 0;
+            //this.counter = 0;
     }
 
     print(){
@@ -22,21 +21,67 @@ class Field {
         let input = prompt('WHICH WAY? ')
         if (input !== 'r' && input !== 'l' && input !== 'd' && input !== 'u') {
             console.log('ERROR: WRONG INPUT!');
-            this.askDirection()
-
+            //this.askDirection()
+            this.next()
         }else {
             //console.log(`your direction is ${input}`)
             return(input)
         }
     }
-    run(){
+    run(multiplier){
+        this.multiplier = multiplier;
+        let randomStart = () => {
+            let rs = Math.floor(Math.random()*this.field.length);
+            let cs = Math.floor(Math.random()*this.field[0].length)
+            if(this.field[rs][cs] === '░' ){
+                this.field[rs][cs] = '*'
+                this.row = rs;
+                this.column = cs;
+            }else{
+                randomStart()
+            }
+        }
+        let randomHat = () => {
+            let rh = Math.floor(Math.random()*this.field.length);
+            let ch = Math.floor(Math.random()*this.field[0].length)
+            if(this.field[rh][ch] === '░'){
+                this.field[rh][ch] = '^'
+            }else{
+                randomHat()
+            }
+        }
+
+        randomStart()
+        randomHat()
         this.print()
-        this.row = 0;
-        this.column = 0;
         this.next()
+    }
+    randomHole(){
+        let rh =0;
+        let ch =0;
+        if(this.row > this.field.length - 2 && this.column > this.field[0].length - 5){
+            rh = this.row - Math.floor(Math.random()*10);
+            ch = this.column - Math.floor(Math.random()*10);
+        }else if( this.row > 2 && this.column > 2){
+            rh = this.row + Math.floor(Math.random()*10);
+            ch = this.column + Math.floor(Math.random()*10);
+        }
+
+        if(this.field[rh][ch] !== '^' && this.field[rh][ch] !== this.field[this.row][this.column]){
+            this.field[rh][ch] = 'O'
+        }else{
+            //this.counter++;
+            this.randomHole();
+        }
+    }
+    XRandomHole(){
+        for(let i=0;i<this.multiplier;i++){
+            this.randomHole();
+        }
     }
     step(){
         this.field[this.row][this.column] = '*';
+        this.XRandomHole()
         this.print()
     }
     win(){
@@ -64,7 +109,6 @@ class Field {
             this.step()
             this.next()
         }
-
     }
 
     next(){
@@ -84,15 +128,39 @@ class Field {
             this.process()
         }
     }
-    generateField(){
 
+    static generateField(height, width, POH){
+        let items = height * width;
+        let numOfHoles = Math.floor(items * POH / 100);
+        let field = [] ;
+
+        let condition = () => {
+            let r = Math.floor(Math.random()*height);
+            let c = Math.floor(Math.random()*width)
+            if(field[r][c] === '░' ){
+                field[r][c] = 'O'
+            }else{
+                condition()
+            }
+        }
+
+        for(let i = 0; i < height; i++){
+            field.push([])
+        }
+        for(let i = 0; i < field.length; i++ ){
+            for(let j = 0; j < width; j++){
+                field[i].push('░')
+            }
+        }
+        for(let i=0; i < numOfHoles; i++){
+            condition();
+        }
+        return field
     }
 
 }
-const myField = new Field([
-    ['*', '░', 'O'],
-    ['░', 'O', '░'],
-    ['░', '^', '░'],
-]);
+//Field.generateField(11,12,5)
 
-myField.run()
+const myField = new Field(Field.generateField(20,60,5));
+
+myField.run(10)
